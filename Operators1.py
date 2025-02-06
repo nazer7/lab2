d7f07cec8095e35131e1aeeaa742561645066392
nazer7 <nazerkebekbosyn932@gmail.com>
2025-02-06 20:45:06 +0300

    Add files via upload

---
+++ Operators1.py
@@ -0,0 +1,11 @@
+print(10 + 5)
+x = 5
+y = 3
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+#the floor division // rounds the result down to the nearest whole number
