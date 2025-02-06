752aca26ec605e62806f15098228e3743ca51d3b
nazer7 <nazerkebekbosyn932@gmail.com>
2025-02-06 20:39:02 +0300

    Add files via upload

---
+++ example6.py
@@ -0,0 +1,13 @@
+def myFunction() :
+  return True
+print(myFunction())
+
+def myFunction2() :
+  return True
+if myFunction2():
+  print("YES!")
+else:
+  print("NO!")
+
+x = 200
+print(isinstance(x, int))
