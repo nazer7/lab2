706becc2df6aa465bd0308c00847100fbceaa8ed
nazer7 <nazerkebekbosyn932@gmail.com>
2025-02-07 15:38:34 +0300

    Add files via upload

---
+++ Access List Items.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+print(thislist[-1])
+thislist2 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist2[2:5])
+print(thislist2[:4])
+print(thislist2[2:])
+print(thislist2[-4:-1])
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
