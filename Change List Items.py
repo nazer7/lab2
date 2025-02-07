d8606fe81ab5c95616bc6eddb3845353ec7ceeb2
nazer7 <nazerkebekbosyn932@gmail.com>
2025-02-07 15:52:38 +0300

    Add files via upload

---
+++ Change List Items.py
@@ -0,0 +1,20 @@
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+thislist2 = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist2[1:3] = ["blackcurrant", "watermelon"]
+print(thislist2)
+
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist) 
+
+
