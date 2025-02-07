2a57f45489630605a2fa27033b8804d389e25f3d
nazer7 <nazerkebekbosyn932@gmail.com>
2025-02-07 16:01:52 +0300

    Add files via upload

---
+++ Add List Items.py
@@ -0,0 +1,16 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+thislist2 = ["apple", "banana", "cherry"]
+thislist2.insert(1, "orange")
+print(thislist2)
+thislist3 = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist3.extend(tropical)
+print(thislist3)
+thislist4 = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist4.extend(thistuple)
+print(thislist4) 
+
+
